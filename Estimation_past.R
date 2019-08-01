4fa0ee9a070a70137dc901be282b3fcc66962960
Andres Baeza <andres.baeza@gmail.com>
2019-08-01 00:41:47 -0700

    Update Estimation_past.R

--- Estimation_past.R
+++ Estimation_past.R
@@ -1,4 +1,3 @@
-require("raster")
 metodo="GLM"
 load("data/raster_Tmean_historic")
 load("data/annual_total_rainfall_raster_historico")
@@ -8,7 +7,10 @@ multiple_rainfall=PPT_m_H*PPT_m_H
 #re-clasification
 m <- c(-1, 0, NA,  0.1, 5000, 1)
 rclmat <- matrix(m, ncol=3, byrow=TRUE)
+rm(m)
+
 rc <- reclassify(PPT_m_H, rclmat)
+
 
 total_annual_rainfall=PPT_m_H*rc
 raster_Tmean_annual_H=raster_Tmean_annual_H*rc
@@ -25,34 +27,25 @@ predictors_past<-stack(raster_Tmean_annual_H,
                   multiple_rainfall
                   )
 
-predictors_past_wo_tmin<-stack(raster_Tmean_annual_H,
-                          total_annual_rainfall,
-                  multiple_rainfall
-)
 
-rm(raster_Tmean_annual)
-rm(raster_Tmin_annual)
+rm(raster_Tmean_annual_H)
+rm(raster_Tmin_annual_H)
 rm(PPT_m_H)
 rm(multiple_rainfall)
 rm(total_annual_rainfall)
 
 llmespg<-cbind(mesquite_pg_clean$lon[which(mesquite_pg_clean$year<=1950)], mesquite_pg_clean$lat[which(mesquite_pg_clean$year<=1950)])
 llmespg<-llmespg[,-which(llmespg[,2]<0)]
-presvals_pg<-as.data.frame(extract(predictors_past, llmespg))
-presvals_pg_wo_tmin<-as.data.frame(extract(predictors_past_wo_tmin, llmespg))
+presvals_pg<-as.data.frame(raster::extract(predictors_past, llmespg))
 names(presvals_pg)<-c("mean_annual_temperature","min_annual_temperature","total_annual_rainfall","multiple_rainfall")
 names(predictors_past)<-names(presvals_pg)
-
-names(presvals_pg_wo_tmin)<-c("mean_annual_temperature","total_annual_rainfall","multiple_rainfall")
-names(predictors_past_wo_tmin)<-names(presvals_pg_wo_tmin)
-
 
 ##########################################################################
 
 #training set (absences)
 #backgr <- dismo::randomPoints(predictors_past, 2000)
 backgr<-readRDS("data/background_points")
-absvals <- as.data.frame(extract(predictors_past, backgr))
+absvals <- as.data.frame(raster::extract(predictors_past, backgr))
 names(absvals)<-names(presvals_pg)
 
 pg <- c(rep(1, nrow(presvals_pg)), rep(0, nrow(absvals)))
@@ -63,28 +56,18 @@ valid_points=which(sdmdata_pg$mean_annual_temperature!="NaN")
 traning<-sample(x = (1:length(sdmdata_pg$mean_annual_temperature))[valid_points],size =round(length(valid_points)/2),replace = F)
 
 traning_point=sdmdata_pg[traning,]
-testing_points=sdmdata_pg[-traning,]
 
-p_training=sdmdata_pg$pg[traning]
-
-
-
-require(MASS)
 #Estimations and predictions
 if (metodo=="GLM"){
-m1 <- glm(pg ~ mean_annual_temperature+multiple_rainfall+total_annual_rainfall, data=traning_point,na.action = na.omit)
-m1_predict=predict(object = m1,newdata=as.data.frame(predictors_past))
 
-m2 <- glm(pg ~ mean_annual_temperature+min_annual_temperature+multiple_rainfall+total_annual_rainfall, data=traning_point,na.action = na.omit)
-m2_predict=predict(object = m2,newdata=as.data.frame(predictors_past))
+model <- glm(pg ~ mean_annual_temperature+min_annual_temperature+multiple_rainfall+total_annual_rainfall, data=traning_point,na.action = na.omit)
+model_predict=predict(object = model,newdata=as.data.frame(predictors_past))
 }
 
 if (metodo=="GAM"){
-m3<-gam::gam(pg ~ mean_annual_temperature+multiple_rainfall+total_annual_rainfall, data=traning_point,na.action = na.omit)
-m3_predict<-gam::predict.Gam(object = m3,newdata=as.data.frame(predictors_past))
 
-m4<-gam::gam(pg ~ mean_annual_temperature+min_annual_temperature+multiple_rainfall+total_annual_rainfall, data=traning_point,na.action = na.omit)
-m4_predict<-gam::predict.Gam(object = m4,newdata=as.data.frame(predictors_past))
+model<-gam::gam(pg ~ mean_annual_temperature+min_annual_temperature+multiple_rainfall+total_annual_rainfall, data=traning_point,na.action = na.omit)
+model_predict<-gam::predict.Gam(object = model,newdata=as.data.frame(predictors_past))
 
 }
 
@@ -92,23 +75,14 @@ m4_predict<-gam::predict.Gam(object = m4,newdata=as.data.frame(predictors_past))
 if (metodo=="BIOCLIM"){
 ###########################################################
 #bioclim uses only- presence data
-valid_points_wo_tmin=which(presvals_pg_wo_tmin$mean_annual_temperature!="NaN")
-traning_wo_tmin<-sample(x = (1:length(presvals_pg_wo_tmin$mean_annual_temperature))[valid_points_wo_tmin],size =round(length(valid_points_wo_tmin)/2),replace = F)
-traning_point_wo_tmin=presvals_pg_wo_tmin[traning_wo_tmin,]
-testing_points_wo_tmin=presvals_pg_wo_tmin[-traning_wo_tmin,]
-
-m5<-dismo::bioclim(traning_point_wo_tmin)
-m5_predict<-predict(m5,predictors_past_wo_tmin)
-
 
 
 valid_points=which(presvals_pg$mean_annual_temperature!="NaN")
 traning<-sample(x = (1:length(presvals_pg$mean_annual_temperature))[valid_points],size =round(length(valid_points)/2),replace = F)
 traning_point=presvals_pg[traning,]
-testing_points=presvals_pg[-traning,]
 
-m6<-dismo::bioclim(traning_point)
-m6_predict<-predict(m6,predictors_past)
+model<-dismo::bioclim(traning_point)
+model_predict<-predict(model,predictors_past)
 }
 ##########################################################################
 
@@ -116,18 +90,7 @@ m6_predict<-predict(m6,predictors_past)
 
 #evaluation
 nn<-raster(x = predictors_past)
-if (metodo=="GLM"){
-dist_m1<-setValues(x = nn,values = m1_predict)
-dist_m2<-setValues(x = nn,values = m2_predict)
-}
-if (metodo=="GAM"){
-dist_m3<-setValues(x = nn,values = m3_predict)
-dist_m4<-setValues(x = nn,values = m4_predict)
-}
-if (metodo=="BIOCLIM"){
-  dist_m5<-m5_predict
-dist_m6<-m6_predict
-}
+dist<-setValues(x = nn,values = model_predict)
 
 ##############################################################################################################
 USA_map_full<-maptools::readShapeSpatial("C:/Users/abaezaca/Dropbox (ASU)/Design Principles in CIS/maps_USA/tl_2017_us_state/tl_2017_us_state")
@@ -139,50 +102,68 @@ USA_map<-crop(USA_map_full,raster::extent(left,right,bottom,top))
 USA_map@data$NAME<-factor(USA_map@data$NAME)
 ##############################################################################################################
 
-if (metodo=="GLM"){
-  dist_m1=mask(dist_m1,USA_map)
-  dist_m2=mask(dist_m2,USA_map)
-}
-if (metodo=="GAM"){
-  dist_m3=mask(dist_m3,USA_map)
-  dist_m4=mask(dist_m4,USA_map)
-}
-if (metodo=="BIOCLIM"){
-  dist_m5=mask(dist_m5,USA_map)
-dist_m6=mask(dist_m6,USA_map)
-}
+dist=mask(dist,USA_map)
 
-if (metodo=="GLM"){
-eval_m1<-dismo::evaluate(p=llmespg[-traning,],a=backgr,model = m1,x=predictors_past)
-eval_m2<-dismo::evaluate(p=llmespg[-traning,],a=backgr,model = m2,x=predictors_past)
-}
-if (metodo=="GAM"){
-eval_m3<-dismo::evaluate(p=llmespg[-traning,],a=backgr,model = m3,x=predictors_past)
-eval_m4<-dismo::evaluate(p=llmespg[-traning,],a=backgr,model = m4,x=predictors_past)
-}
-
-if (metodo=="BIOCLIM"){
-  eval_m5<-dismo::evaluate(p=llmespg[-traning_wo_tmin,],a=backgr,model = m5,x=predictors_past_wo_tmin)
-eval_m6<-dismo::evaluate(p=llmespg[-traning,],a=backgr,model = m6,x=predictors_past)
-}
+eval<-dismo::evaluate(p=llmespg[-traning,],a=backgr,model = model,x=predictors_past)
 
 
-source("future_scenarios.R")
+#source("future_scenarios.R")
 nn_future<-raster(x = predictors_future)
 
-if (metodo=="GLM"){
-  T50<-dist_m2>eval_m2@t[which.max(eval_m2@TPR+eval_m2@TNR)]
-  m2_predict_T2000=predict(object = m2,newdata=as.data.frame(predictors_current))
-  dist_m2<-setValues(x = nn,values = m2_predict_T2000)
-  T2000<-dist_m2>eval_m2@t[which.max(eval_m2@TPR+eval_m2@TNR)]
+  T50 <- dist>eval@t[which.max(eval@TPR+eval@TNR)]
+  m2_predict_T2000 <- predict(object = model,newdata=as.data.frame(predictors_current))
+  dist_T2000 <- setValues(x = nn,values = m2_predict_T2000)
+  T2000 <- dist_T2000>eval@t[which.max(eval@TPR+eval@TNR)]
   
-  m2_predict_future_T2100=predict(object = m2,newdata=as.data.frame(predictors_future))
-  dist_m2_future<-setValues(x = nn_future,values = m2_predict_future_T2100)
-  dist_m2_future=mask(dist_m2_future,USA_map)
-  T2100<-dist_m2_future>eval_m2@t[which.max(eval_m2@TPR+eval_m2@TNR)]
-}
+  m2_predict_T2100 <- predict(object = model,newdata=as.data.frame(predictors_future))
+  dist_T2100 <- setValues(x = nn_future,values = m2_predict_T2100)
+  dist_T2100 <- mask(dist_T2100,USA_map)
+  T2100<-dist_T2100>eval@t[which.max(eval@TPR+eval@TNR)]
 
-
+  
+  
+  T2000%>%
+    disaggregate(fact=res(T2000)/res(T50))->T2000
+  crs(T2000) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84"
+  
+  T50 %>% crop(T2000) -> T50
+  crs(T50) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84"
+  
+  T2000 <- resample(T2000,T50)
+  plot(T2000 - T50 > 0.5)
+  
+  
+  T2100%>%
+    disaggregate(fact=res(T2100)/res(T50))->T2100
+  crs(T2100) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84"
+  
+  T2100 <- resample(T2100,T50)
+  plot(T2100- T50 > 0.5)
+  
+  rm(sdmdata_pg)
+  rm(presvals_pg)
+  rm(valid_points)
+  rm(llmespg)
+  rm(nn)
+  rm(nn_future)
+  rm(eval)
+  rm(m2_predict_T2100)
+  rm(m2_predict_T2000)
+  rm(dist)
+  rm(dist_T2100)
+  rm(dist_T2000)
+  rm(backgr)
+  rm(traning_point)
+  rm(model_predict)
+  rm(rclmat)
+  rm(model)
+  rm(rclmat)
+  rm(USA_map_full)
+  rm(pg)
+  rm(traning)
+  rm(rc)
+  rm(mesquite_pg_clean)
+  rm(absvals)
 ###############################################################################3
 # png(filename = "output_maps/comparison_withTmin_past.png",width = 20,height = 30,res=300,units = "cm")
 # par(mfrow=c(3,2))
